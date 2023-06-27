1b1dd82244a61fb87dcae700a9fd08f0bccf569d
ignaarguello <igna.arguello2110@hotmail.com>
2023-06-27 16:04:15 -0300

    Commit: 5 - Primeras configuraciones y archivos

---
+++ src/config.py
@@ -0,0 +1,10 @@
+class DevelopmentConfig():
+    DEBUG = True
+    MYSQL_HOST:'localhost'
+    MYSQL_USER:'root'
+    MYSQL_HOST:'123456'
+    MYSQL_HOST:'api_flask'
+    
+config = {
+    'development': DevelopmentConfig,
+}
