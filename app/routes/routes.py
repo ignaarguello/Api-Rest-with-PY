a83926590dbab07354128796583492b9ddebd138
ignaarguello <igna.arguello2110@hotmail.com>
2023-06-27 13:28:07 -0300

    Commit: 02 - Primeros archivos

---
+++ app/routes/routes.py
@@ -0,0 +1,15 @@
+from app import app
+
+
+@app.route('/')
+def hello():
+    return 'Mi Primera app con flask perri'
+
+
+@app.route('/products')
+def helloo():
+    return 'Productos disponibles'
+
+
+if __name__ == '__main__':
+    app.run()
